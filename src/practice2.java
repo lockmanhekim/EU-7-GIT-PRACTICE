742c498e2ddb7926b02749d8bd39f78e7dbad2a1
aynurozge <ozgeaynur1@gmail.com>
2022-02-05 18:49:44 +0300

    second commıt

--- src/practice2.java
+++ src/practice2.java
@@ -1,5 +1,7 @@
 public class practice2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Yusuf");
+        System.out.println("Aynur");
     }
 }
